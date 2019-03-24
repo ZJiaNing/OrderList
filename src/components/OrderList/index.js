f55b7554cc52e46722ebdf984242bf1d6b2f66a2
ZJiaNing <ada733@outlook.com>
2019-03-24 21:56:25 +0800

    add reference

---
+++ src/components/OrderList/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import OrderItem from '../OrderItem';
+class OrderList extends Component {
+    render() {
+        return (
+            <div>
+                <OrderItem />
+            </div>
+        );
+    }
+}
+
+export default OrderList;
